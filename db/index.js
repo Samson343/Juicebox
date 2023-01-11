2198a6ac978e1d6e4e6beb806168163347ddc0c8
Samuel <sam.ban@gmail.com>
2023-01-11 12:49:36 -0700

    finished step onepart

--- db/index.js
+++ db/index.js
@@ -10,8 +10,24 @@ async function getAllUsers () {
   return rows
 }
 
+async function createUser({ username, password }) {
+  try {
+    const result = await client.query(`
+       INSERT INTO users(username, password)
+       VALUES ($1, $2)
+       ON CONFLICT (username) DO NOTHING
+       RETURNING *;
+    `, [username, password])
+
+    return result.rows
+  } 
+  catch (error) {
+    throw error
+  }
+}
 
 module.exports = {
     client,
     getAllUsers,
+    createUser
   }
--- db/seed.js
+++ db/seed.js
@@ -1,28 +1,96 @@
 // grab our client with destructuring from the export in index.js
-const { client, getAllUsers } = require('./index');
 
+const {
+  client,
+  getAllUsers,
+  createUser
+} = require('./index');
+
+
+async function createInitialUsers() {
+  // const albertTwo = await createUser({ username: 'albert', password: 'imposter_albert' })
+  try {
+    console.log("Starting to create users...")
+
+    const albert = await createUser({ username: 'albert', password: 'bertie99' })
+    const sandra = await createUser ({ username: 'sandra', password: '2sandy4me' })
+    const glamgal = await createUser ({ username: 'glamgal', password: 'soglam' })
+
+    console.log(albert)
+    console.log("finished creating users!")
+  }
+  catch (error) {
+    console.error("error creating users!")
+    throw error
+  }
+}
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers()
+  } catch (error) {
+    throw error;
+  }
+}
 
 async function testDB() {
-    try {
-      // connect the client to the database, finally
-      client.connect();
-  
-      // queries are promises, so we can await them
-      const users = await getAllUsers()
-      //   const result = await client.query(`SELECT * FROM users;`);
-  
-      // for now, logging is a fine way to see what's up
+  try {
+    console.log("Starting to test database...");
 
-      console.log(users);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      // it's important to close out the client connection
-      client.end();
-    }
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
   }
-  
-  testDB();
+}
+
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
+
 
   //queries must end with a semicolon
 // \l to see your database
@@ -33,3 +101,5 @@ async function testDB() {
 // DELETE * FROM
 // nmp i pg to start postgress in your node project
 // \q to quit postgres
+// to drop a table essentially means deleting it
+// psql -U postgres to start postgress with the username posgres
