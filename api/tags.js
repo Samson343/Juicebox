d5f561f44fece7b8ab2e2bb99cd201caf8e9edd4
Samson343 <sam.ban@gmail.com>
2023-01-22 13:18:38 -0700

    Everything working

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const tagsRouter = express.Router();
 const { getAllTags, getPostsByTagName } = require('../db');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -13,11 +15,21 @@ tagsRouter.get('/:tagName/posts', async (req, res, next) => {
   // read the tagname from the params
   const { tagName } = req.params;
   // could also be written this way- const tagNames = req.params.tagName
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+  const token = auth.slice(prefix.length);
 
   try {
     // use our method to get posts by tag name from the db
-    const posts = await getPostsByTagName(tagName)
+    const allPosts = await getPostsByTagName(tagName)
+    const { id } = jwt.verify(token, JWT_SECRET);
     // send out an object to the client { posts: // the posts }
+
+    const  posts = allPosts.filter(post => {
+      return post.active && (post.author.id === id)
+    });
+
+
     res.send({ posts: posts })
 
   } catch ({ name, message }) {
